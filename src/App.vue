12e342a6584d56fb824f0fb57899c641d218e221
liyl <2416053677@qq.com>
2024-03-11 16:44:11 +0800

    『005. 一个简单的效果』 总结：Vue3向下兼容Vue2语法

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
-    <div class="app">
-        <h1>Hello World!</h1>
-    </div>
+  <div class="app">
+    <h1>Hello World!</h1>
+    <person />
+  </div>
 </template>
 
 <script lang="ts">
+import person from "./components/person.vue";
 export default {
-    name:'App'
-}
+  name: "App",
+  components: { person }, //注册组件
+};
 </script>
 
 <style>
 .app {
-    background-color: #ddd;
-    box-shadow: 0 0 10px;
-    border-radius: 10px;
-    padding: 20px;
+  background-color: #ddd;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
 }
 </style>
---
+++ src/components/person.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="person">
+    <h4>姓名：{{ name }}</h4>
+    <h4>年龄：{{ age }}</h4>
+    <h4>性别：{{ sex }}</h4>
+    <button @click="changeName">修改名字</button>
+    <button @click="changeAge">修改年龄</button>
+    <button @click="showTel">查看联系方式</button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "person",
+  data() {
+    return {
+      name: "张三",
+      age: 19,
+      sex: "男",
+      tel: 123123123,
+    };
+  },
+  methods: {
+    changeName() {
+      this.name = "zhang-san"
+    },
+    changeAge() {
+      this.age +=1
+    },
+    showTel() {
+      alert(this.tel);
+    },
+  },
+};
+</script>
+
+<style>
+.person {
+  background-color: skyblue;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+button{
+  margin: 0 10px;
+}
+</style>
