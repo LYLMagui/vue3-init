2c5abfc17c4f202a23daf473650ddc7fcca347bb
liyl <2416053677@qq.com>
2024-03-11 11:30:09 +0800

    初始化项目

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-// 导入依赖，相当于导入Spring的包
-import { createApp } from 'vue'
-// 引入样式
-import './style.css'
-// 引入根组件
-import App from './App.vue'
-// 创建前端应用，传入根组件，并挂载到id为'app'的容器里，这个容器在index.html文件中
-//相当于SpringApplication.run(xxx.class)创建Spring容器
-createApp(App).mount('#app')
