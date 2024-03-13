7746b32991f08030544b7b010ab3dd8262d3ae36
liyl <2416053677@qq.com>
2024-03-13 11:02:25 +0800

    009 setup与OptionsAPI

--- src/components/person.vue
+++ src/components/person.vue
@@ -11,6 +11,12 @@
 <script lang="ts">
 export default {
   name: "person",
+  data() {
+    return {
+      //可以读取到setup里的数据
+      a: this.name
+    }
+  },
   setup() {
     //数据
     let name = "张三"
@@ -21,18 +27,15 @@ export default {
     function changeName() {
       name = "zhang-san"
     }
-
     function changeAge() {
       age += 1
     }
-
     function showTel() {
       alert(tel)
     }
-
-    return {name,age,changeName,changeAge,showTel}
-
+    return { name, age, changeName, changeAge, showTel }
+    //setup的返回值也可以是一个渲染函数
+    //return () => "setup渲染的函数"
   }
-  
 };
 </script>
