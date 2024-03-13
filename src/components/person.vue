c4f27a44631fba631e3081696503ea28dfad4494
liyl <2416053677@qq.com>
2024-03-13 22:14:44 +0800

    『016. computed计算属性』

--- src/components/person.vue
+++ src/components/person.vue
@@ -1,37 +1,39 @@
 <template>
-  <div class="game">
-    <h2>我买了一辆{{ car.name }},花了{{ car.price }}万</h2>
-    <button @click="changeCar">没钱了</button>
-    <hr>
-    <h2>游戏列表：</h2>
-    <ul>
-      <li v-for='g in games' :key='g.id'>{{ g.name }}</li>
-    </ul>
-    <button @click="changeGameName">改变游戏名</button>
+  <div class="person">
+    姓：<input type="text" v-model="firstName" />
+    名：<input type="text" v-model="lastName" />
+    全名；<span>{{ fullName }}</span>
+    <button @click="changeName">改名</button>
   </div>
 </template>
 
-<script lang="ts" setup name="game">
-import { reactive, ref } from 'vue';
+<script lang="ts" setup name="person">
+import { ref,computed } from "vue";
 
-//reactive定义对象
-let car = reactive({
-  name: "帕拉梅拉",
-  price:100
+// 数据 
+let firstName = ref('zhang')
+let lastName = ref("san")
+
+// 这样定义的fullName变量是只读不可修改的
+// let fullName = computed(() => {
+//   return firstName.value.slice(0,1).toUpperCase() + firstName.value.slice(1) + '-' + lastName.value
+// })
+
+// 这种方法定义的fullName才是可读可写的
+let fullName = computed({
+  get() {
+    return firstName.value.slice(0, 1).toUpperCase() + firstName.value.slice(1) + '-' + lastName.value
+  },
+  set(val) {
+    const [str1, str2] = val.split('-')
+    firstName.value = str1
+    lastName.value = str2
+  }
 })
 
-// 使用ref创建响应式对象
-let games = ref([
-  { id: "g1", name: "战地5" },
-  { id: "g2", name: "梦幻西游" },
-  { id: "g3", name: "原神" },
-])
+function changeName() {
+  fullName.value = "li-si"
+}
 
-function changeGameName() {
-  games.value[0].name = "我被笨蛋美女包围了！"
-}
-function changeCar() {
-  //使用Object.assign()将新的对象赋值给原对象，如果是ref定义的对象，可以直接赋值：car = {name:"奥迪A8",price:150}
-  Object.assign(car, { name:"奥迪A8",price:150})
-}
+
 </script>
