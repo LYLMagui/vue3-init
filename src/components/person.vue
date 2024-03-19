441d94d8fa42b207d3da315f509cacc513792d8f
liyl <2416053677@qq.com>
2024-03-19 23:17:34 +0800

    『029. 自定义hooks』

--- src/components/person.vue
+++ src/components/person.vue
@@ -1,27 +1,26 @@
 <template>
   <div class="person">
-    <ul>
-      <!-- 如果不使用唯一标识的话，要用默认的索引，写法如下 -->
-      <!-- <li v-for="(p,index) in list" :key="index">{{ p.name }} -- {{ p.age }}</li> -->
-      <li v-for="p in list" :key="p.id">{{ p.name }} -- {{ p.age }}</li>
-    </ul>
+    <h2>当前求和为：{{ sum }}</h2>
+    <button @click="add">点我sum+1</button>
+    <hr>
+    <img v-for="(dog, index) in dogList" :src="dog" :key="index">
+    <button @click="getDog">再来一直小狗</button>
   </div>
 </template>
 
 <script lang="ts" setup name="person">
-import { Persons } from '../types';
+// 引入hooks
+import useSum from '../hooks/useSum';
+import useDog from '../hooks/useDog';
 
-// @@值接收类型@@
-// 接收父组件传的值，同时把props保存起来
-let x = defineProps(["list"])
-
-// @@接收list + 限制类型@@
-defineProps<{ list: Persons }>()
-
-// @@接收list + 限制类型 + 限制必要性 + 指定默认类型@@
-withDefaults(defineProps<{ list?: Persons }>(), {
-  list: () => [{ id: "asdfg0", name:"赵一",age:10}]
-})
-
+// 解构对象
+const { dogList, getDog } = useDog()
+const { sum, add } = useSum()
 
 </script>
+<style scoped>
+img {
+  height: 100px;
+  margin-right: 10px;
+}
+</style>
