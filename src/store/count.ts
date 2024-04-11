94ae15b1fdc4f8e1f27f6f3ee3006e8ce689aa1e
liyl <2416053677@qq.com>
2024-04-11 17:48:18 +0800

    『47. 修改数据的三种方式』

--- src/store/count.ts
+++ src/store/count.ts
@@ -2,6 +2,16 @@ import { defineStore } from "pinia";
 
 // 参数：1.id名，和文件名一致 2.配置对象
 export const useCountStore = defineStore('count', {
+	// acitons里放置的是一个一个的方法，用于响应组件中的动作
+	actions: {
+		incremnet(value:number) {
+			if (this.sum <= 10) {
+				this.sum += value;
+				
+			}
+			
+		}
+	},
 	state() {
 		return {
 			sum:0
