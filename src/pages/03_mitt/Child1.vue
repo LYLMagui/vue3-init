3dd9f5b3df78ad477023c2ac1ac93d028ae25aa2
liyl <2416053677@qq.com>
2024-04-16 16:15:45 +0800

    『54.组件通信  mitt

--- src/pages/03_mitt/Child1.vue
+++ src/pages/03_mitt/Child1.vue
@@ -1,23 +1,43 @@
 <template>
-  <div class="child1">
-    <h3>子组件1</h3>
+	<div class="child1">
+		<h3>子组件1</h3>
+		<h4>手机：{{ phone }}</h4>
+		<h4>借来的电脑：{{ computer }}</h4>
+		<button @click="emitter.emit('send-phone',phone)">借出手机</button>
 
-  </div>
+	</div>
 </template>
 
 <script setup lang="ts" name="Child1">
-	
+import { ref } from 'vue'
+import emitter from '../../utils/emitter';
+
+//数据
+const phone = ref('小米');
+const computer = ref('')
+
+emitter.on('send-computer', (value:any) => {
+	computer.value = value
+})
+
+
+
+
+
+
+
 </script>
 
 <style scoped>
-	.child1{
-		margin-top: 50px;
-		background-color: skyblue;
-		padding: 10px;
-		box-shadow: 0 0 10px black;
-		border-radius: 10px;
-	}
-	.child1 button{
-		margin-right: 10px;
-	}
+.child1 {
+	margin-top: 50px;
+	background-color: skyblue;
+	padding: 10px;
+	box-shadow: 0 0 10px black;
+	border-radius: 10px;
+}
+
+.child1 button {
+	margin-right: 10px;
+}
 </style>
--- src/pages/03_mitt/Child2.vue
+++ src/pages/03_mitt/Child2.vue
@@ -1,19 +1,33 @@
 <template>
-  <div class="child2">
-    
-  </div>
+	<div class="child2">
+		<h3>子组件2</h3>
+		<h4>电脑：{{ computer }}</h4>
+		<h4>借来的手机：{{ phone }}</h4>
+		<button @click="emitter.emit('send-computer',computer)">借出电脑</button>
+	</div>
 </template>
 
 <script setup lang="ts" name="Child2">
+import { ref } from 'vue'
+import emitter from '../../utils/emitter';
+
+
+// 数据
+const computer = ref('华硕');
+const phone = ref('')
+
+emitter.on('send-phone', (value:any) => {
+	phone.value = value;
+})
 
 </script>
 
 <style scoped>
-	.child2{
-		margin-top: 50px;
-		background-color: orange;
-		padding: 10px;
-		box-shadow: 0 0 10px black;
-		border-radius: 10px;
-	}
+.child2 {
+	margin-top: 50px;
+	background-color: orange;
+	padding: 10px;
+	box-shadow: 0 0 10px black;
+	border-radius: 10px;
+}
 </style>
