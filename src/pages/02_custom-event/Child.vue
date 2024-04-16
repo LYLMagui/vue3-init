2b48f8e9d252a18f92b9744e8556302bda137ef2
liyl <2416053677@qq.com>
2024-04-16 13:55:48 +0800

    『53.组件通信 自定义事件』

--- src/pages/02_custom-event/Child.vue
+++ src/pages/02_custom-event/Child.vue
@@ -1,21 +1,29 @@
 <template>
-  <div class="child">
-    <h3>子组件</h3>
-	<h4>父亲给的车：{{ car }}</h4>
-  </div>
+	<div class="child">
+		<h3>子组件</h3>
+		<h4>儿子的玩具：{{ toy }}</h4>
+		<button @click="emit('send-toy', toy)">把玩具送给父亲</button>
+	</div>
 </template>
 
 <script setup lang="ts" name="Child">
-import { defineProps } from 'vue';
-defineProps(["car"]);	
+import { ref } from 'vue';
+// 数据
+
+const toy = ref("奥特曼");
+
+// 声明事件
+const emit = defineEmits(["send-toy"])
+
+
 </script>
 
 <style scoped>
-	.child{
-		margin-top: 10px;
-		background-color: rgb(76, 209, 76);
-		padding: 10px;
-		box-shadow: 0 0 10px black;
-		border-radius: 10px;
-	}
+.child {
+	margin-top: 10px;
+	background-color: rgb(76, 209, 76);
+	padding: 10px;
+	box-shadow: 0 0 10px black;
+	border-radius: 10px;
+}
 </style>
--- src/pages/02_custom-event/Father.vue
+++ src/pages/02_custom-event/Father.vue
@@ -1,21 +1,34 @@
 <template>
-  <div class="father">
-    <h3>父组件</h3>
-	
-  </div>
+	<div class="father">
+		<h3>父组件</h3>
+		<h4>儿子给的玩具：{{ toy }}</h4>
+		<Child @send-toy="getToy"/>
+	</div>
 </template>
 
 <script setup lang="ts" name="Father">
-  
+import { ref } from 'vue'
+import Child from './Child.vue';
+
+// 数据
+const toy = ref("");
+
+// 方法
+function getToy(value:string) {
+	toy.value = value;
+}
+
+
 </script>
 
 <style scoped>
-	.father{
-		background-color:rgb(165, 164, 164);
-		padding: 20px;
-    border-radius: 10px;
-	}
-	.father button{
-		margin-right: 5px;
-	}
+.father {
+	background-color: rgb(165, 164, 164);
+	padding: 20px;
+	border-radius: 10px;
+}
+
+.father button {
+	margin-right: 5px;
+}
 </style>
