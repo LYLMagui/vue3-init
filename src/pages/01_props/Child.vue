7433a7285019b91b539cf02abefde6692a037bc0
liyl <2416053677@qq.com>
2024-04-16 10:53:29 +0800

    『52. 组件通信 props』

---
+++ src/pages/01_props/Child.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="child">
+		<h3>子组件</h3>
+		<h4>儿子的玩具：{{ toy }}</h4>
+		<h4>父亲送的车：{{ car }}</h4>
+		<button @click="sendToy(toy)">送给父亲一个玩具</button>
+	</div>
+</template>
+
+<script setup lang="ts" name="Child">
+import { ref } from "vue";
+
+const toy = ref('奥特曼')
+defineProps(["car","sendToy"])
+</script>
+
+<style scoped>
+.child {
+	background-color: skyblue;
+	padding: 10px;
+	box-shadow: 0 0 10px black;
+	border-radius: 10px;
+}
+</style>
