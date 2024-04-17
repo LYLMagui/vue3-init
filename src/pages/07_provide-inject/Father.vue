77e5b01340bb0a593a6d03849573be34327dc806
liyl <2416053677@qq.com>
2024-04-17 17:57:00 +0800

    『58. 组件通信 provide-inject』

--- src/pages/07_provide-inject/Father.vue
+++ src/pages/07_provide-inject/Father.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="father">
     <h3>父组件</h3>
-   
+    <h4>美元：{{ money }}万</h4>
+    <h4>车子：价值{{ car.brand }}万美元的{{ car.price }}</h4>
+    <Child />
   </div>
 </template>
 
 <script setup lang="ts" name="Father">
- 
+import Child from './Child.vue'; 
+import { ref, reactive, provide } from 'vue';
+
+const money = ref(100);
+const car = reactive({
+  brand: "小米",
+  price: 32
+})
+
+function getMoney(value:number) {
+  money.value -= value
+}
+
+// 向所有的后代提供数据
+provide('moneyContext', {money,getMoney})
+provide('car', car)
+
+
 </script>
 
 <style scoped>
-  .father {
-    background-color: rgb(165, 164, 164);
-    padding: 20px;
-    border-radius: 10px;
-  }
+.father {
+  background-color: rgb(165, 164, 164);
+  padding: 20px;
+  border-radius: 10px;
+}
 </style>
--- src/pages/07_provide-inject/GrandChild.vue
+++ src/pages/07_provide-inject/GrandChild.vue
@@ -1,19 +1,28 @@
 <template>
   <div class="grand-child">
-    <h3>我是孙组件</h3>
-  
+    <h4>美元：{{ money }}万</h4>
+    <h4>车子：价值{{ car.brand }}万美元的{{ car.price }}</h4>
+    <button @click="getMoney(5)">花了爷爷的钱</button>
   </div>
 </template>
 
 <script setup lang="ts" name="GrandChild">
-  
+import { inject } from 'vue'
+// 这里需要给默认值，不然会报错
+let { money, getMoney } = inject("moneyContext",{money:0,getMoney:(x:number) => {x}})
+
+let car = inject("car", { brand: "华为", price: 80 })
+
+
+
+
 </script>
 
 <style scoped>
-  .grand-child{
-    background-color: orange;
-    padding: 20px;
-    border-radius: 10px;
-    box-shadow: 0 0 10px black;
-  }
+.grand-child {
+  background-color: orange;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 0 10px black;
+}
 </style>
